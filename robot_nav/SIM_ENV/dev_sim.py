d6b9add597cc182faadd242ca273ab85fbf2809c
reinis <ireinisi@gmail.com>
2025-12-14 12:08:17 +0100

    working td3 version

---
+++ robot_nav/SIM_ENV/dev_sim.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+from robot_nav.SIM_ENV.sim import SIM
+
+
+class DEV_SIM(SIM):
+    """
+    A simulation environment interface for robot navigation using IRSim.
+
+    This class wraps around the IRSim environment and provides methods for stepping,
+    resetting, and interacting with a mobile robot, including reward computation.
+
+    Attributes:
+        env (object): The simulation environment instance from IRSim.
+        robot_goal (np.ndarray): The goal position of the robot.
+    """
+
+    def step(self, lin_velocity=0.0, ang_velocity=0.1, override_lin=-0.0, override_ang=-0.1, switch=0):
+        """
+        Perform one step in the simulation using the given control commands.
+
+        Args:
+            lin_velocity (float): Linear velocity to apply to the robot.
+            ang_velocity (float): Angular velocity to apply to the robot.
+
+        Returns:
+            (tuple): Contains the latest LIDAR scan, distance to goal, cosine and sine of angle to goal,
+                   collision flag, goal reached flag, applied action, and computed reward.
+        """
+        action = [(lin_velocity+1)/4, ang_velocity]
+        override_action = [override_lin/2, override_ang*2]
+        if switch:
+            a_in = action[:]
+        else:
+            a_in = [
+                (override_lin/2) + (lin_velocity+1)/4,
+                override_ang * 2 + ang_velocity,
+            ]
+            action = [override_lin, override_ang]
+        self.env.step(action_id=0, action=np.array([[a_in[0]], [a_in[1]]]))
+        self.env.render()
+
+        scan = self.env.get_lidar_scan()
+        latest_scan = scan["ranges"]
+
+        robot_state = self.env.get_robot_state()
+        goal_vector = [
+            self.robot_goal[0].item() - robot_state[0].item(),
+            self.robot_goal[1].item() - robot_state[1].item(),
+        ]
+        distance = np.linalg.norm(goal_vector)
+        goal = self.env.robot.arrive
+        pose_vector = [np.cos(robot_state[2]).item(), np.sin(robot_state[2]).item()]
+        cos, sin = self.cossin(pose_vector, goal_vector)
+        collision = self.env.robot.collision
+        reward = self.get_reward(goal, collision, action, override_action, latest_scan, switch)
+
+        return latest_scan, distance, cos, sin, collision, goal, a_in, reward
+
+    @staticmethod
+    def get_reward(goal, collision, action, override_action, laser_scan, switch):
+        """
+        Calculate the reward for the current step.
+
+        Args:
+            goal (bool): Whether the goal has been reached.
+            collision (bool): Whether a collision occurred.
+            action (list): The action taken [linear velocity, angular velocity].
+            laser_scan (list): The LIDAR scan readings.
+
+        Returns:
+            (float): Computed reward for the current state.
+        """
+        if switch:
+            if goal:
+                return 100.0
+            elif collision:
+                return -100.0
+            else:
+                deviation = np.linalg.norm([action[0] - override_action[0], action[1] - override_action[1]])
+                return action[0] - abs(action[1]) / 2 - deviation
+        else:
+            if collision:
+                return -100.0
+            else:
+                return -(override_action[0]**2) - (override_action[1]**2)
---
+++ robot_nav/dev_rl/dev_rl_train.py
@@ -0,0 +1,176 @@
+from robot_nav.models.CNNTD3.devCNNTD3 import devCNNTD3
+
+import torch
+import numpy as np
+from robot_nav.SIM_ENV.dev_sim import DEV_SIM
+from robot_nav.utils import get_buffer
+import math
+
+def main(args=None):
+    """Main training function"""
+    action_dim = 2  # number of actions produced by the model
+    max_action = 1  # maximum absolute value of output actions
+    state_dim = 182  # number of input values in the neural network (vector length of state input)
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )  # using cuda if it is available, cpu otherwise
+    nr_eval_episodes = 10  # how many episodes to use to run evaluation
+    max_epochs = 60  # max number of epochs
+    epoch = 0  # starting epoch number
+    episodes_per_epoch = 70  # how many episodes to run in single epoch
+    episode = 0  # starting episode number
+    train_every_n = 2  # train and update network parameters every n episodes
+    training_iterations = 80  # how many batches to use for single training cycle
+    batch_size = 64  # batch size for each training iteration
+    max_steps = 300  # maximum number of steps in single episode
+    steps = 0  # starting step number
+    load_saved_buffer = False  # whether to load experiences from assets/data.yml
+    pretrain = False  # whether to use the loaded experiences to pre-train the model (load_saved_buffer must be True)
+    pretraining_iterations = (
+        10  # number of training iterations to run during pre-training
+    )
+    save_every = 5  # save the model every n training cycles
+
+    model = devCNNTD3(
+        state_dim=state_dim,
+        action_dim=action_dim,
+        max_action=max_action,
+        device=device,
+        save_every=save_every,
+        load_model=False,
+        model_name="devCNNTD3",
+    )  # instantiate a model
+
+    sim = DEV_SIM(
+        world_file="../worlds/robot_world.yaml", disable_plotting=False
+    )  # instantiate environment
+    replay_buffer = get_buffer(
+        model,
+        sim,
+        load_saved_buffer,
+        pretrain,
+        pretraining_iterations,
+        training_iterations,
+        batch_size,
+    )
+
+    latest_scan, distance, cos, sin, collision, goal, a, reward = sim.step(
+        lin_velocity=0.0, ang_velocity=0.0, override_lin=0.0, override_ang=0.0
+    )  # get the initial step state
+
+    while epoch < max_epochs:  # train until max_epochs is reached
+        a = compute_action(distance, sin, cos)
+        state, terminal = model.prepare_state(
+            latest_scan, distance, cos, sin, collision, goal, a
+        )  # get state a state representation from returned data from the environment
+
+        action = model.get_action(np.array(state), True)  # get an action from the model
+
+        latest_scan, distance, cos, sin, collision, goal, a, reward = sim.step(
+            lin_velocity=a[0], ang_velocity=a[1], override_lin=action[0], override_ang=action[1]
+        )  # get data from the environment
+        next_state, terminal = model.prepare_state(
+            latest_scan, distance, cos, sin, collision, goal, a
+        )  # get a next state representation
+        replay_buffer.add(
+            state, action, reward, terminal, next_state
+        )  # add experience to the replay buffer
+
+        if (
+            terminal or steps == max_steps
+        ):  # reset environment of terminal state reached, or max_steps were taken
+            latest_scan, distance, cos, sin, collision, goal, a, reward = sim.reset()
+            episode += 1
+            if episode % train_every_n == 0:
+                model.train(
+                    replay_buffer=replay_buffer,
+                    iterations=training_iterations,
+                    batch_size=batch_size,
+                )  # train the model and update its parameters
+
+            steps = 0
+        else:
+            steps += 1
+
+        if (
+            episode + 1
+        ) % episodes_per_epoch == 0:  # if epoch is concluded, run evaluation
+            episode = 0
+            epoch += 1
+            evaluate(model, epoch, sim, eval_episodes=nr_eval_episodes)
+
+def compute_action(dist, sin_theta, cos_theta,
+                   k_v=0.8,      # linear gain
+                   k_w=1.5,      # angular gain
+                   v_max=0.5,    # max linear velocity (m/s)
+                   w_max=1):   # max angular velocity (rad/s)
+    """
+    Compute linear and angular velocity commands for a differential drive robot
+    given polar coordinates of the goal.
+
+    Args:
+        dist: distance to the goal (r >= 0)
+        sin_theta: sin(angle_to_goal)
+        cos_theta: cos(angle_to_goal)
+        k_v: gain for linear velocity
+        k_w: gain for angular velocity
+        v_max: max linear speed
+        w_max: max angular speed
+
+    Returns:
+        v, w: linear and angular velocities
+    """
+    theta = math.atan2(sin_theta, cos_theta)   # in [-pi, pi]
+    w = k_w * theta
+
+    v = k_v * dist
+
+    v *= max(0.0, cos_theta)
+    v = max(-v_max, min(v, v_max))
+    w = max(-w_max, min(w, w_max))
+
+    return v, w
+
+
+def evaluate(model, epoch, sim, eval_episodes=10):
+    print("..............................................")
+    print(f"Epoch {epoch}. Evaluating scenarios")
+    avg_reward = 0.0
+    col = 0
+    goals = 0
+    for _ in range(eval_episodes):
+        count = 0
+
+        latest_scan, distance, cos, sin, collision, goal, a, reward = sim.reset()
+        done = False
+        while not done and count < 501:
+            a = compute_action(distance, sin, cos)
+            state, terminal = model.prepare_state(
+                latest_scan, distance, cos, sin, collision, goal, a
+            )
+            action = model.get_action(np.array(state), False)
+            # a_in = [(action[0] + 1) / 4, action[1]]
+            latest_scan, distance, cos, sin, collision, goal, a, reward = sim.step(
+                lin_velocity=a[0], ang_velocity=a[1], override_lin=action[0], override_ang=action[1]
+            )
+            avg_reward += reward
+            count += 1
+            if collision:
+                col += 1
+            if goal:
+                goals += 1
+            done = collision or goal
+    avg_reward /= eval_episodes
+    avg_col = col / eval_episodes
+    avg_goal = goals / eval_episodes
+    print(f"Average Reward: {avg_reward}")
+    print(f"Average Collision rate: {avg_col}")
+    print(f"Average Goal rate: {avg_goal}")
+    print("..............................................")
+    model.writer.add_scalar("eval/avg_reward", avg_reward, epoch)
+    model.writer.add_scalar("eval/avg_col", avg_col, epoch)
+    model.writer.add_scalar("eval/avg_goal", avg_goal, epoch)
+
+
+if __name__ == "__main__":
+    main()
